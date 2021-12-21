49c0dc545de41fae63f0f39beee7a0b0884669f8
suajo <75597824+suajo@users.noreply.github.com>
2021-12-22 00:20:01 +0900

    py

---
+++ Python/prime num.py
@@ -0,0 +1,27 @@
+# 소수 찾기
+
+# A
+# def is_prime_num(x):
+#     for i in range(2, x):
+#         if x % i == 0:
+#             return False
+#     return True
+
+
+# print(is_prime_num(5))
+# print(is_prime_num(8))
+
+import math
+
+
+def is_prime_num(x):
+    # 2부터 x의 '제곱근'까지의 모든 수 확인
+    for i in range(2, int(math.sqrt(x))+1):
+        if x % i == 0:
+            return False
+
+    return True
+
+
+print(is_prime_num(5))
+print(is_prime_num(8))
