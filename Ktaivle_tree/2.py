9b7be64f018f312ad83d896f16e18cc9f9a411f8
suajo <75597824+suajo@users.noreply.github.com>
2021-12-22 12:23:24 +0900

    tree3

--- Ktaivle_tree/2.py
+++ Ktaivle_tree/2.py
@@ -1,11 +1,16 @@
 x = int(input())
+# x > 5
 
 print('\n'*2)
 
 for i in range(x+1):
     if i == 1:
         print(' '*5 + ' '*(x-i) + '🟎')
+    elif i % 5 == 0:
+        print(' '*5 + ' '*(x-(i-1)) + '⋆'*(2*(i-1)))
     else:
         print(' '*5 + ' '*(x-i) + '⁂'*(2*i-1))
+for k in range(2):
+    print(' '*(x+2) + '│││││')
 
-print('\n'*2)
+print('\n'*8)
