7d8a8f9a7c8f12a334138eaba80dbd104748b20e
suajo <75597824+suajo@users.noreply.github.com>
2021-12-22 18:13:10 +0900

    tree4

--- Ktaivle_tree/2.py
+++ Ktaivle_tree/2.py
@@ -1,17 +1,32 @@
-x = int(18)
+x = int(25)
 
 print('\n'*2)
 
-for i in range(x+1):
+for i in range(8):
     if i == 1:
-        print(' '*5 + ' '*(x-i) + '\u001b[33m🟎')
+        print(' '*10 + ' '*(x-i) + '\u001b[33m🟎')
     elif i % 4 == 0:
-        print(' '*5 + ' '*(x-(i-1)) + '\u001b[33m⋆'*(2*(i-1)))
+        print(' '*10 + ' '*(x-(i-1)) + '\u001b[33m⋆'*(2*(i-1)))
     else:
-        print(' '*5 + ' '*(x-i) + '\u001b[32m⁂'*(2*i-1))
-for k in range(3):
-    print(' '*(x+2) + '\u001b[37m│││││')
+        print(' '*10 + ' '*(x-i) + '\u001b[32m⁂'*(2*i-1))
+for i in range(8, 16):
+    if i == 1:
+        print(' '*10 + ' '*(x-i) + '\u001b[33m🟎')
+    elif i % 4 == 0:
+        print(' '*10 + ' '*(x-(i-1)+3) + '\u001b[33m⋆'*(2*(i-1-3)))
+    else:
+        print(' '*10 + ' '*(x-i+3) + '\u001b[32m⁂'*(2*i-1-6))
+for i in range(16, x+1):
+    if i == 1:
+        print(' '*10 + ' '*(x-i) + '\u001b[33m🟎')
+    elif i % 4 == 0:
+        print(' '*10 + ' '*(x-(i-1)+6) + '\u001b[33m⋆'*(2*(i-1-6)))
+    else:
+        print(' '*10 + ' '*(x-i+6) + '\u001b[32m⁂'*(2*i-1-12))
 
-print('￣'*23)
+for k in range(3):
+    print(' '*(x+7) + '\u001b[37m│││││')
+
+print('￣'*35)
 
 print('\n'*8)
